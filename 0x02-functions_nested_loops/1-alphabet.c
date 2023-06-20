d844a66fd8248cf7c3a0076397f88565b12c1352
mourouane <elqasemymarouane716@gmail.com>
2023-06-20 19:43:06 +0000

    b

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main: Entry point
+* Desciption: print_alphabe
+* Return: 0
+*/
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
