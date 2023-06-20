e1c030f21955fa995ef25ce62c363df927d2a60d
mourouane <elqasemymarouane716@gmail.com>
2023-06-20 19:50:34 +0000

    b

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
-* main: Entry point
-* Desciption: print_alphabe
-* Return: 0
+* print_alphabet - print all alphabet in lowercase
 */
 void print_alphabet(void)
 {
 char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++);
 _putchar(letter);
 _putchar('\n');
 }
