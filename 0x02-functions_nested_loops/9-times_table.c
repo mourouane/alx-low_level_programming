f2bc83cb628f72a8a1a0c0e62e298adc8f542704
mourouane <elqasemymarouane716@gmail.com>
2023-06-21 20:28:41 +0000

    game

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
 */
 void times_table(void)
 {
-int i, j;
+int i, j,
 int k;
 for (i = 0; i < 10; i++)
 {
@@ -22,7 +22,8 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} else if (k >= 10)
+}
+else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
